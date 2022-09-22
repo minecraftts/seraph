db2d7bc9e914c9749dfe174717b6440626cac48c
zani <61130784+Zxnii@users.noreply.github.com>
2022-09-21 21:40:57 -0600

    exports: export everything in single file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,58 @@
+import Constants from "./Constants";
+import Display from "./Display";
+import DisplayEvents from "./DisplayEvents";
+import DeletedError from "./errors/DeletedError";
+import GlewInitializationError from "./errors/GlewInitializationError";
+import NotImplementedError from "./errors/NotImplementedError";
+import NotInitializedError from "./errors/NotInitializedError";
+import SeraphInitializationError from "./errors/SeraphInitializationError";
+import Monitor from "./Monitor";
+import Camera from "./objects/cameras/Camera";
+import PerspectiveCamera from "./objects/cameras/PerspectiveCamera";
+import Mesh from "./objects/Mesh";
+import Scene from "./objects/Scene";
+import BufferAttribTypes from "./renderer/BufferAttribTypes";
+import BufferAttribute from "./renderer/BufferAttribute";
+import Material from "./renderer/materials/Material";
+import MaterialOptions from "./renderer/materials/MaterialOptions";
+import MaterialUniformType from "./renderer/materials/MaterialUniformType";
+import UnlitMaterial from "./renderer/materials/UnlitMaterial";
+import Renderer from "./renderer/Renderer";
 import Seraph from "./Seraph";
+import StateManager from "./StateManager";
+import GLUtil from "./util/GLUtil";
+import MathUtil from "./util/MathUtil";
 
-export default Seraph;
+export {
+    Seraph,
+
+    Constants,
+    Display,
+    DisplayEvents,
+    Monitor,
+    StateManager,
+
+    DeletedError,
+    GlewInitializationError,
+    NotImplementedError,
+    NotInitializedError,
+    SeraphInitializationError,
+
+    Camera,
+    PerspectiveCamera,
+
+    Mesh,
+    Scene,
+
+    Material,
+    MaterialOptions,
+    MaterialUniformType,
+    UnlitMaterial,
+
+    BufferAttribTypes,
+    BufferAttribute,
+    Renderer,
+
+    GLUtil,
+    MathUtil
+};
