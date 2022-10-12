069ef56efbbdc8207925dcff603a5325fff4aafa
zani <zxnimh@gmail.com>
2022-10-12 17:43:32 -0600

    chore: document Constants.ts

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,5 +1,8 @@
 import path from "path";
 
 export default class Constants {
+    /**
+     * The directory where built-in Seraph shaders are stored
+     */
     public static SERAPH_SHADER_DIR: string = path.join(__dirname, "../shaders");
 }
