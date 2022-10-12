2293957f6737d6ba4623d1cb1f6ca19b27f4250d
zani <zxnimh@gmail.com>
2022-10-12 17:42:17 -0600

    chore: document BufferAttribute.ts

--- src/renderer/BufferAttribute.ts
+++ src/renderer/BufferAttribute.ts
@@ -1,6 +1,9 @@
 import { GL_FLOAT } from "@minecraftts/opengl";
 import BufferAttribTypes from "./BufferAttribTypes";
 
+/**
+ * Specifies an attribute on a mesh
+ */
 export default class BufferAttribute {
     private data: Float32Array = new Float32Array(0);
 
@@ -16,26 +19,45 @@ export default class BufferAttribute {
         this.normalize = normalize;
     }
 
+    /**
+     * Sets the attribute's data
+     * @param buffer the new buffer
+     */
     public setBuffer(buffer: Float32Array) {
         this.data = buffer;
     }
 
+    /**
+     * @returns the attribute's data
+     */
     public getBuffer(): Float32Array {
         return this.data;
     }
 
+    /**
+     * @returns the width of a single element
+     */
     public getWidth(): number {
         return this.width;
     }
 
+    /**
+     * @returns the attribute's type 
+     */
     public getType(): BufferAttribTypes {
         return this.type;
     }
 
+    /**
+     * @returns the attribute's location
+     */
     public getPosition(): number {
         return this.position;
     }
 
+    /**
+     * @returns whether or not the attribute is normalized 
+     */
     public isNormalized(): boolean {
         return this.normalize;
     }
