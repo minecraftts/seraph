248a1fd2c5d76c8f1032754da4c0536516ed7c77
zani <zxnimh@gmail.com>
2022-10-12 15:55:03 -0600

    chore: document Seraph.ts

--- src/Seraph.ts
+++ src/Seraph.ts
@@ -5,10 +5,16 @@ import SeraphInitializationError from "./errors/SeraphInitializationError";
 import NotInitializedError from "./errors/NotInitializedError";
 import Versions from "./Versions";
 
+/**
+ * Main class for Seraph. `Seraph.initialize()` must be called before calling any other Seraph code.
+ */
 export default class Seraph {
     private static initialized: boolean = false;
     private static glewInitialized: boolean = false;
 
+    /**
+     * Initialize Seraph
+     */
     public static initialize() {
         if (!glfwInit()) {
             throw new SeraphInitializationError("failed to initialize glfw");
@@ -17,6 +23,9 @@ export default class Seraph {
         this.initialized = true;
     }
 
+    /**
+     * Frees any resources used by Seraph
+     */
     public static cleanup() {
         if (!this.initialized) {
             throw new NotInitializedError();
@@ -28,11 +37,18 @@ export default class Seraph {
         this.glewInitialized = false;
     }
 
+    /**
+     * Checks if Seraph is initialized
+     * @returns {boolean} `true` if Seraph is initialized, `false` otherwise
+     */
     public static isInitialized(): boolean {
         return this.initialized;
     }
 
-    public static setGlewInit() {
+    /**
+     * Sets flag telling Seraph GLEW is initialized. You probably shouldn't call this unless you're manually doing window / OpenGL context creation.
+     */
+    public static setGlewInit(): void {
         if (!this.initialized) {
             throw new NotInitializedError();
         }
@@ -40,6 +56,14 @@ export default class Seraph {
         this.glewInitialized = true;
     }
 
+    /**
+     * Returns versions for all libraries
+     * 
+     * `versions.glfw` is always present and is set to the GLFW version exposed by `@minecraftts/glfw`
+     * `versions.glew` is present only after initializing GLEW and an OpenGL context. It is set to the GLEW version exposed by `@minecraftts/opengl`
+     * `versions.opengl` is present only after initializing an OpenGL context. It is set to your system's OpenGL version or the context's OpenGL version.
+     * @returns {Versions} an object containing versions for glfw, glew and opengl
+     */
     public static getVersions(): Versions {
         if (!this.initialized) {
             throw new NotInitializedError();
