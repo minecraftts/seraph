e8c703eee174ab5ca5064082b8fcc84b1600251b
zani <zxnimh@gmail.com>
2022-10-12 16:02:00 -0600

    chore: document StateManager.ts

--- src/StateManager.ts
+++ src/StateManager.ts
@@ -1,11 +1,18 @@
 import { glfwSwapInterval } from "@minecraftts/glfw";
 import { glBindTexture, glUseProgram } from "@minecraftts/opengl";
 
+/**
+ * General utility class to manage the OpenGL state
+ */
 export default class StateManager {
     private static currentProgram: number = -1;
     private static currentTextures: Record<number, number> = {};
     private static vsync: boolean;
 
+    /**
+     * Switches to specified program unless it's already active.
+     * @param program an opengl program
+     */
     public static useProgram(program: number): void {
         if (this.currentProgram === program) {
             return;
@@ -14,6 +21,11 @@ export default class StateManager {
         glUseProgram(program);
     }
 
+    /**
+     * Binds the current texture unless it is already in use
+     * @param target the target to bind to. only `GL_TEXTURE2D` is currently supported
+     * @param texture an opengl texture
+     */
     public static bindTexture(target: number, texture: number): void {
         if (this.currentTextures[target] === texture) {
             return;
@@ -24,6 +36,10 @@ export default class StateManager {
         this.currentTextures[target] = texture;
     }
 
+    /**
+     * Sets vsync on/off for all windows
+     * @param state a boolean indicating whether vsync should be on or off
+     */
     public static setVsync(state: boolean): void {
         if (this.vsync !== state) {
             this.vsync = state;
@@ -31,6 +47,11 @@ export default class StateManager {
         }
     }
 
+    /**
+     * Gets the bound texture for the target.
+     * @param target the target to query
+     * @returns the currently bound opengl texture for the target or undefined if none is bound
+     */
     public static getCurrentTexture(target: number): number | undefined {
         return this.currentTextures[target];
     }
