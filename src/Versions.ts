1a371422eceae14efe84d4b6a75d724a49c64021
zani <zxnimh@gmail.com>
2022-10-12 16:06:28 -0600

    chore: document Versions.ts

--- src/Versions.ts
+++ src/Versions.ts
@@ -1,3 +1,12 @@
+/**
+ * Object containing versions.
+ * 
+ * `versions.glfw` is the glfw version exposed by `@minecraftts/glfw`
+ * 
+ * `versions.glew` is only present if GLEW has been initialized and an OpenGL context exists. It is set to the GLEW version exposed by `@minecraftts/opengl`
+ * 
+ * `versions.opengl` is only present if an OpenGL context exists. It is set to the context version or your system's OpenGL version.
+ */
 type Versions = {
     glew?: string;
     glfw: string;
