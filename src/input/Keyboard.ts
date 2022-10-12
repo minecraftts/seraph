83b136ecb6b7c397d05d51c225d79633f3db83d3
zani <zxnimh@gmail.com>
2022-10-12 17:43:16 -0600

    chore: document Keyboard.ts

--- src/input/Keyboard.ts
+++ src/input/Keyboard.ts
@@ -42,14 +42,26 @@ export default class Keyboard extends (EventEmitter as new () => TypedEventEmitt
         }
     }
 
+    /**
+     * @param keycode the key to check press state for
+     * @returns `true` if the key defined by `keycode` is currently pressed, `false` otherwise 
+     */
     public getKeyDown(keycode: number): boolean {
         return this.keyStates.has(keycode);
     }
 
+    /**
+     * @param keycode the key to get state for
+     * @returns a `KeyState` if the key defined by `keycode` is currently pressed, `undefined` otherwise
+     */
     public getKeyState(keycode: number): KeyState | undefined {
         return this.keyStates.get(keycode);
     }
 
+    /**
+     * @param key the key to get name for 
+     * @returns `string` if the key has a name, `undefined` otherwise
+     */
     public static getKeyName(key: number): string | undefined {
         if (key >= 39 && key <= 96) {
             return glfwGetKeyName(key, 0);
