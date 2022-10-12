deed26a0e7c4f9c4c5bc2560f194c42aab49a1f9
zani <zxnimh@gmail.com>
2022-10-12 17:41:04 -0600

    chore: document MathUtil.ts

--- src/util/MathUtil.ts
+++ src/util/MathUtil.ts
@@ -1,4 +1,9 @@
 export default class MathUtil {
+    /**
+     * Helper function to convert degrees to radians
+     * @param deg degrees
+     * @returns radians
+     */
     public static degreesToRad(deg: number): number {
         return deg * (Math.PI / 180);
     }
