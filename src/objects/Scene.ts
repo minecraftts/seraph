e51bac123efe85debc38eb74a84c095b89250f23
zani <zxnimh@gmail.com>
2022-10-12 17:43:08 -0600

    chore: document Scene.ts

--- src/objects/Scene.ts
+++ src/objects/Scene.ts
@@ -7,16 +7,27 @@ export default class Scene {
 
     }
 
+    /**
+     * Adds mesh `element` to the scene
+     * @param element mesh to add
+     */
     public add(element: Mesh): void {
         this.elements.push(element);
     }
 
+    /**
+     * Removes mesh `element` from the scene
+     * @param element mesh to remove
+     */
     public remove(element: Mesh): void {
         const index: number = this.elements.findIndex(val => val === element);
 
         this.elements.splice(index, 1);
     }
 
+    /**
+     * @returns a list of all meshes present within the scene
+     */
     public getElements(): Mesh[] {
         return this.elements;
     }
