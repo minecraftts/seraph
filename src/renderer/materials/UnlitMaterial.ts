bf4db278486fc949171e6e5466282aa2b884efc3
zani <zxnimh@gmail.com>
2022-10-23 17:47:25 -0600

    feat: add tex_bound to the registered uniforms

--- src/renderer/materials/UnlitMaterial.ts
+++ src/renderer/materials/UnlitMaterial.ts
@@ -3,6 +3,7 @@ import path from "path";
 import Constants from "../../Constants";
 
 type Uniforms = {
+    tex_bound: "int",
     view_matrix: "mat4x4",
     model_matrix: "mat4x4",
     projection_matrix: "mat4x4"
@@ -18,6 +19,7 @@ export default class UnlitMaterial extends Material<Uniforms> {
             fragmentPath: path.join(Constants.SERAPH_SHADER_DIR, "unlit.fsh")
         });
 
+        this.registerUniform("tex_bound", "int");
         this.registerUniform("view_matrix", "mat4x4");
         this.registerUniform("model_matrix", "mat4x4");
         this.registerUniform("projection_matrix", "mat4x4");
